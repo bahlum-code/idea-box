bd263e289d91caa06bf6360e49c9324ed2035b7f
gonzalo <gonzalojpv@gmail.com>
2021-08-26 00:09:33 -0500

    GP, first component

--- src/App.vue
+++ src/App.vue
@@ -20,49 +20,28 @@
         </form>
       </section>
       <!-- /.Add Idea -->
-      <article class="p-4 rounded-lg bg-gray-300 sm:flex sm:items-center">
-        <!-- Info -->
-        <section class="text-center sm:flex-1 sm:text-left">
-          <h2 class="text-xl sm:leading-6 sm:text-2xl">
-            this is idea title must be collapsed on another row
-          </h2>
-          <small>Username</small>
-        </section>
-        <!-- /.Info -->
-        <!-- Vote -->
-        <section
-          class="
-            pt-3
-            border-t-2
-            mt-6
-            border-black
-            sm:pt-0
-            sm:pl-3
-            sm:border-t-0
-            sm:border-l-2
-            sm:mt-0
-            sm:flex
-            sm:items-center
-          "
-        >
-          <h3 class="text-3xl font-bold text-center">99</h3>
-          <nav class="flex justify-center sm:block">
-            <img
-              class="w-10 cursor-pointer"
-              src="@/assets/images/arrow.svg"
-              alt="Vote Up"
-            />
-            <img
-              class="w-10 cursor-pointer transform rotate-180"
-              src="@/assets/images/arrow.svg"
-              alt="Vote down"
-            />
-          </nav>
-        </section>
-        <!-- /.Vote -->
-      </article>
+      <AppIdea v-for="(idea, $index) in ideas" :key="$index" :idea="idea" />
     </div>
     <!-- /.Main box -->
   </div>
   <!-- /.Main container -->
 </template>
+
+<script>
+import AppIdea from "@/components/AppIdea";
+import seed from "./seed.json";
+import { ref } from "vue";
+
+export default {
+  components: {
+    AppIdea,
+  },
+  setup() {
+    const ideas = ref(seed.ideas);
+
+    return {
+      ideas,
+    };
+  },
+};
+</script>
